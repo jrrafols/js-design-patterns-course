43185693cbc3aaf939e41e9a55b7c05007d8bbf3
jrrafols <jr.rafols@gmail.com>
2016-02-15 16:21:45 -0700

    Completion of exercise

--- Clicker/clicker.js
+++ Clicker/clicker.js
@@ -1,36 +1,56 @@
-
-
-var Clicker = function(options){
-    var numClicks = 0;
+(function() {
+    var Clicker = function(options){
+        var numClicks = 0;
+        
+        var container = document.createElement('div');
+        
+        var heading = document.createElement('h3');
+        heading.textContent = options.name;
+        
+        var image = document.createElement('img');
+        image.src = options.src;
+        image.width = 300;
+        image.alt = options.name;
+        
+        var clickCount = document.createElement('p');
+        clickCount.innerHTML = "The image has been clicked <span class=\"clicks\">0</span> times."
+        
+        container.appendChild(heading);
+        container.appendChild(image);
+        container.appendChild(clickCount);
+        
+        var count = clickCount.querySelector('.clicks');
+        
+        image.addEventListener('click', function(){
+            numClicks += 1;
+            count.textContent = numClicks.toString();
+        }, false);
+        
+        var clickerLink = document.querySelector('a[data-name=' + options.name + ']');
+        var clickerContainer = document.querySelector('#ClickerContainer');
+        clickerLink.addEventListener('click', function(){
+            while (clickerContainer.firstChild) {
+                clickerContainer.removeChild(clickerContainer.firstChild);
+            }
+            clickerContainer.appendChild(container);
+        });
+    };
     
-    var container = document.createElement('div');
+    function populateList(data){
+        var list = document.querySelector('body ul');
+        
+        data.forEach(function(item){
+           var listItem = document.createElement('li');
+           var linkName = document.createElement('a');
+           linkName.href = "#";
+           linkName.dataset.name = item.name;
+           linkName.textContent = item.name;
+           
+           listItem.appendChild(linkName);
+           list.appendChild(listItem); 
+        });
+    }
     
-    var heading = document.createElement('h3');
-    heading.textContent = options.name;
-    
-    var image = document.createElement('img');
-    image.src = options.src;
-    image.width = 300;
-    image.alt = options.name;
-    
-    var clickCount = document.createElement('p');
-    clickCount.innerHTML = "The image has been clicked <span class=\"clicks\">0</span> times."
-    
-    container.appendChild(heading);
-    container.appendChild(image);
-    container.appendChild(clickCount);
-    
-    document.querySelector('body').appendChild(container);
-    
-    var count = clickCount.querySelector('.clicks');
-    
-    image.addEventListener('click', function(){
-        numClicks += 1;
-        count.textContent = numClicks.toString();
-    }, false);
-};
-
-(function(){
     var data = [
         {
             name: "Lucky1",
@@ -54,6 +74,7 @@ var Clicker = function(options){
         },
     ]
     
+    populateList(data);
     data.forEach(function(item){
         new Clicker(item); 
     });
